39301c13d4a85b7251f693c6de1311d0bacaaf7f
jonelle-noelani <jonelle.noelani@gmail.com>
2021-05-09 15:10:37 -0700

    search box and button icon

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const OMDb = "http://www.omdbapi.com/?i=tt3896198&apikey=3c28a15d"
+const OMDb2 = "http://www.omdbapi.com/?s=inception&apikey=3c28a15d"
 
 const body = document.querySelector('body')
 const p1 = document.createElement('p')
@@ -10,13 +11,14 @@ function testThis(words){
     body.appendChild(p1)
 }
 
-// getMovies(OMDb)
+getMovies('inception')
 
-// function getMovies(OMDb){
-//     console.log("hello");
-    // fetch(OMDb)
-    // .then(res => res.json( ))
+function getMovies(title){
+    console.log("hello");
+    fetch('http://www.omdbapi.com/?s='+title+'&apikey=3c28a15d')
+    .then(res => res.json( ))
+    .then(movies => console.log(movies))
     // .then(movies => movies.forEach(movie => {
     //     console.log(movie)
     // }))
-// }
+}
